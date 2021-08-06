055063d89ecd830e44a5ce6a134b21aaa6983ead
luciana-sarachu <2278@holbertonschool.com>
2021-08-06 13:35:45 -0300

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,6 +9,14 @@ void display_complex_number(complex c)
 {
 	if (c.re && c.im)
 	{
+		if (c.re < 0)
+		{
+			printf("-%g + %gi\n", c.re, c.im);
+		}
+		if (c.im < 0)
+		{
+			printf("%g - %gi\n", c.re, c.im);
+		}
 		printf("%g + %gi\n", c.re, c.im);
 	}
 	else if (c.re && !c.im)
